e35f1d40f3a926bd51bfd2b8ce24b8b4ebebdc7d
jpdino123 <joao.pedro.ferreira11@escola.pr.gov.br>
2024-10-24 11:50:33 -0300

    Add files via upload

---
+++ projeto (1)/perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Biologia',
+    'O que é um organimos heterótrofico?',
+    'precisa de outro seres pra viver'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um virus?',
+    'Parasitas unicelulares, que dependem de outras celulas para se reproduzir'
+)
+
+criaCartao(
+    'Biologia',
+    'Como as plantas se alimentão?',
+    'Por meio da fotosintese'
+)
+criaCartao(
+    'Biologia',
+    'O que é uma bacteria?',
+    'As bactérias são organismos microscópicos unicelulares que podem causar infecções'
+)
+criaCartao(
+    'Biologia',
+    'O que é Meiose',
+    'Meiose é o processo de divisão celular através do qual uma célula tem o seu número de cromossomos reduzido pela metade'
+)
+criaCartao(
+    'Biologia',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
